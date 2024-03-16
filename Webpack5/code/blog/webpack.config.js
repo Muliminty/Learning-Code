788c98f09be4edee2bd0d13127341e29ed8ce223
Muliminty <313195303@qq.com>
2024-03-16 23:01:11 +0800

    feat: HtmlWebpackPlugin基本使用

--- Webpack5/code/blog/webpack.config.js
+++ Webpack5/code/blog/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development', // 指定Webpack的工作模式是开发模式
@@ -7,6 +9,13 @@ module.exports = {
     filename: 'dist.js', // 指定Webpack输出的文件名
     path: path.resolve(__dirname, 'dist') // 指定Webpack输出的目录路径
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo', // 指定生成的HTML文件的标题
+      // template: 'src/index.html', // 指定模板文件路径
+      filename: 'index.html', // 指定生成的 HTML 文件名
+    }),
+  ],
   module: { // 配置Webpack的模块处理规则
     rules: [
       {
@@ -19,5 +28,6 @@ module.exports = {
         type: 'asset/resource'// 指定使用的loader，用于处理图片文件
       }
     ]
-  }
+  },
+
 };
